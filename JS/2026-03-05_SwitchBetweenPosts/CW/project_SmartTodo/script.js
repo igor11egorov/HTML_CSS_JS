b345195fd9776eb8b77a5c564ff906d4adecad31
IEgorov <@i.egorov>
2026-03-06 09:09:29 +0100

    project smart TODO

--- JS/2026-03-05_SwitchBetweenPosts/CW/project_SmartTodo/script.js
+++ JS/2026-03-05_SwitchBetweenPosts/CW/project_SmartTodo/script.js
@@ -1,7 +1,6 @@
 // Smart Todo (1 список)
 // Данные
 // Массив объектов:
-
 // // пример
 // { id: 1, text: "Buy milk", done: false, createdAt: 1710000000 }
 
@@ -60,3 +59,109 @@
 // Без localStorage
 // Без сортировок
 // Все изменения только через функции: add/toggle/delete/clear/getVisible/render
+
+{
+  const key = 'todosStorage'
+
+  const todos = []
+
+  function renderTodos(arr) {
+    const todos = arr ?? getTodos()
+    if (todos && Array.isArray(todos)) {
+      const todoList = document.querySelector('.todo-list')
+      todoList.innerHTML = ''
+      todos.forEach((item) => {
+        // const startDate = new Date(item.startDate).toLocaleString("ru-RU", {
+        //     day: "numeric",
+        //     month: "long",
+        //     hour: "numeric",
+        //     minute: "numeric",
+        // });
+        const id = item.id
+        const element = `
+                        <li class="todo-block">
+                        <label class="checkbox" htmlFor="${id}" onClick="toggleTodoDone('${id}')">
+                            <input type="checkbox" name="${id}" id="${id}" ${item.done ? 'checked' : ''}/>
+                            <span class="material-symbols-rounded checkbox__check-icon">
+                                check
+                            </span>
+                        </label>
+                        <div class="todo-block__data">
+<!--                            <p class="todo-block__date">${startDate}</p>-->
+                            <h3 class="todo-block__title">${item.description}</h3>
+                        </div>
+                        <span class="material-symbols-rounded"
+<!--                         onClick="deleteTodo('${id}')-->
+                         ">
+                            close
+                        </span>
+                        </li>
+                    `
+        todoList.insertAdjacentHTML('beforeend', element)
+      })
+    }
+  }
+
+  function toggleTodoDone(id) {
+    const todos = getTodos()
+
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          done: !todo.done,
+        }
+      }
+      return todo
+    })
+    saveTodos(updatedTodos)
+    // filterAndRenderTodos()
+  }
+
+  function getTodos() {
+    return JSON.parse(localStorage.getItem(key)) ?? []
+  }
+
+  function saveTodos(todos) {
+    localStorage.setItem(key, JSON.stringify(todos))
+  }
+
+
+
+
+  const form = document.querySelector('.create-form')
+  const error = document.querySelector('.error');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const description = document
+      .querySelector('#description').value.trim()
+  })
+
+  function createTodo(text) {
+    return new Todo(text)
+  }
+
+  function addTodo(todos, text) {
+    if (text.length <= 3) {
+        error.classList.toogle('hidden')
+    }
+    todos.push(createTodo(text))
+  }
+
+  class Todo {
+    static counter = 0
+
+    constructor(description) {
+      this.id = ++Todo.counter
+      this.description = description
+      this.createdAt = Date.now()
+      this.done = false
+    }
+  }
+
+//   const todo1 = new Todo('12123')
+//   console.log(todo1)
+//   const todo2 = new Todo('2222')
+//   console.log(todo2)
+}
